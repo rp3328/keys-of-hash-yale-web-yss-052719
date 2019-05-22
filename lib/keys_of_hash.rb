0406c80bd406835b6a6109bfb3c4cf8a69105716
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 12:02:42 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     arr = []
-    temp.each do |key, val|
+    self.each do |key, val|
       if arguments.include?(val)
         arr << keys_of
       end
